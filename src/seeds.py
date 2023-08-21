f5741e7dcf4277c7d12954e90c80ccb11e84dc6d
Chuba <cgmoganedi@gmail.com>
2023-08-21 07:46:11 +0200

    put the seeds in place

--- src/seeds.py
+++ src/seeds.py
@@ -1,25 +1,26 @@
 
-from beverage_machine import IngredientManager
 from models import Ingredient
 
-def seed_ingredients(session):
+def seed_ingredients(session)-> None:
     # Ingredient names and their initial quantities
     initial_ingredients = [
-        {'name': 'water', 'max_capacity': 750, 'quantity': 750, 'unit_of_measure': 'ml'},
-        {'name': 'coffee_beans', 'max_capacity': 250, 'quantity': 250, 'unit_of_measure': 'grams'},
-        {'name': 'tea_bags', 'max_capacity': 50, 'quantity': 50, 'unit_of_measure': 'init'},
-        {'name': 'milk', 'max_capacity': 250, 'quantity': 250, 'unit_of_measure': 'ml'}
+        {'name': 'water', 'max_capacity': 750, 'unit_of_measure': 'ml'},
+        {'name': 'coffee_beans', 'max_capacity': 250, 'unit_of_measure': 'grams'},
+        {'name': 'tea_bags', 'max_capacity': 50, 'unit_of_measure': 'unit'},
+        {'name': 'milk', 'max_capacity': 250, 'unit_of_measure': 'ml'}
     ]
 
-    # Fill ingredients in the database
-    for ingredient_data in initial_ingredients:
-        db_ingredient = Ingredient(**ingredient_data)
-        session.add(db_ingredient)
-    session.commit()
+    try:
+        # Fill ingredients in the database
+        for ingredient_data in initial_ingredients:
+            db_ingredient = Ingredient(**ingredient_data)
+            session.add(db_ingredient)
+        session.commit()
+    except ValueError as e:
+        print('Could not seed.', e)
     
 
-def refill_all_ingredients(session):
-    ingredient_manager = IngredientManager(session)
+def refill_all_ingredients(ingredient_manager)-> str:
     ingredients = [
         {'name': 'water'},
         {'name': 'coffee_beans'},
@@ -27,5 +28,11 @@ def refill_all_ingredients(session):
         {'name': 'milk'}
     ]
     
-    for ingredient in ingredients:
-        ingredient_manager.refill_ingredient(ingredient.name)
+    try:
+        for ingredient in ingredients:
+            ingredient_manager.refill_ingredient(ingredient['name'])
+            
+        return 'Successfully refilled all the ingredients'
+    
+    except ValueError as e:
+        print('Could not re-fill.', e)
